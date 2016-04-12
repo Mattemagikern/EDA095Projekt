339122977fbcaea3e480a35ce5a2d5c5552a0357
Mattemagikern <mansgariusson@gmail.com>
2016-04-12 14:30:11 +0200

    SetUp

---
+++ Projekt/src/Lab3/Conection.java
@@ -0,0 +1,11 @@
+package Lab3;
+
+public class Conection {
+
+//skapar en förbindelse mellan en anvndare och en hub. 
+// En hub är en broadcast användare som fungerar som server funktionallitet
+	// alla användare ansluter till en hub och huben broadcastar alla meddelanden den får från en användare. 
+	// #Bingo
+	
+	
+}
---
+++ Projekt/src/Lab3/Message.java
@@ -0,0 +1,6 @@
+package Lab3;
+
+public class Message {
+//text, hub || användare 
+	
+}
